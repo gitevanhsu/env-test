fdd27eec7c6f69cc93e67732e1487a32841c9217
Evan <aqwe78965@gmail.com>
2023-01-27 08:31:08 +0800

    update yml env file

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <h1>安安安安安安安安安安安安安安安安</h1>
       <>{console.log(import.meta.env.VITE_TEST)}</>
-      <>ver1</>
+      <>ver2</>
       <p>{import.meta.env.VITE_TEST}</p>
       <div>
         <a href="https://vitejs.dev" target="_blank">
