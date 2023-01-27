801776c322b18e43eccc9ad208749a4f2cba2851
Evan <aqwe78965@gmail.com>
2023-01-27 08:17:34 +0800

    show env on page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
+      <h1>安安安安安安安安安安安安安安安安</h1>
+      {console.log(import.meta.env.VITE_TEST)}
+      <p>{import.meta.env.VITE_TEST}</p>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
@@ -28,7 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
